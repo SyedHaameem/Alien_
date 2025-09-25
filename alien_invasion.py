9a51101cc9d4c63e95c270b3eda2ea172c94cb4b
SyedHaameem <syedhaameem20@gmail.com>
2025-09-25 16:23:44 +0530

    update

--- alien_invasion.py
+++ alien_invasion.py
@@ -38,6 +38,8 @@ class AlienInvasion:
 
             elif event.type == pygame.KEYDOWN:
                 self._check_keydown_events(event)
+
+                
                
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
@@ -50,8 +52,8 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True 
 
-        elif event.key == pygame.k_q:
-            sys.exit()  
+        elif event.key == pygame.K_q:
+            sys.exit() 
 
     def _check_keyup_events(self,event):
          """Respond to key releases"""
